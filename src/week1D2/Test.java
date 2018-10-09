3d708f5b230bc53e38f0ce93d8001d550ffb9835
chris <new826@gmail.com>
2018-10-09 14:06:32 -0400

    another test

---
+++ src/week1D2/Test.java
@@ -0,0 +1,7 @@
+package week1D2;
+
+public class Test {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ src/week1D2/w1D2.java
@@ -0,0 +1,10 @@
+package week1D2;
+
+public class w1D2 {
+// Notes for class on 10/9/18
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
